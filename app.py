722a6bc8aefd689d0d4a45eef34c7064a172ae30
dazzledave <gomadodavid6@gmail.com>
2025-06-28 14:41:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,6 @@ def results():
     return render_template('results.html', results=all_results, query=query, selected_scrapers=selected_scrapers)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
